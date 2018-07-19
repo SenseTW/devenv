69d8a7120e53a086156d1428e9d61a1d8503ef27
YWChen <wenchen.x@gmail.com>
2018-07-20 00:28:41 +0800

    Fixed Seed data

--- docker/pg/2-h.sql
+++ docker/pg/2-h.sql
@@ -30,20 +30,6 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
-
-
---
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-
-
---
 -- Name: authclient_grant_type; Type: TYPE; Schema: public; Owner: sensetw
 --
 
