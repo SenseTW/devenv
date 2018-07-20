df1c5d1dc4fec03f196d2cc9f0ad729a1647f609
YWChen <wenchen.x@gmail.com>
2018-07-20 13:57:57 +0800

    Fixed runtime bugs

--- docker/h/entrypoint.sh
+++ docker/h/entrypoint.sh
@@ -8,5 +8,15 @@ if [ ! -f /var/lib/hypothesis/build/manifest.json ]; then
     npm cache clean
 fi
 
+while ! pg_isready -h db -p 5432 > /dev/null 2> /dev/null; do
+    echo "Waiting for psql://db:5432/ ..."
+    sleep 3
+done
 
+until $(curl --output /dev/null --silent --head --fail http://elastic:9200); do
+    printf 'Waiting for Elastic http://elastic:9200/ ...'
+    sleep 3
+done
+
+hypothesis init
 init-env supervisord -c /supervisord.conf
